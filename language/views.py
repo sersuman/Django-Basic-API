54d93c0c43443f43514746a0864520dff6e2c2f6
sersuman <sumanshrestha670@gmail.com>
2020-03-12 08:53:47 +0545

    basic api

---
+++ language/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Language, Programmer, Paradigm
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    # when permission is needed in this view only
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+    
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
